b36f8496cfea0bf97a91b3ea5f1230879e4ec754
Ross Jameson <Rossjameson1623@gmail.com>
2022-07-23 14:00:42 -0500

    add third web sdk

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// import { AppProps } from "next/app";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
+// the chain id 4 represents the rinkeby network and
+//the injected connector is a web 3 connection method used by meta mask
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
