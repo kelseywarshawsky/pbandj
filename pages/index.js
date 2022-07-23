16cb40035e59f6136bff1e2899b96b3df0546422
Blake Wright <btdubbzzz@gmail.com>
2022-07-23 17:15:44 -0500

    fixed build error in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { useTheme } from '@mui/material';
-import { useWeb3 } from '@3rdweb/hooks';
-import Header from '../components/Header';
-import Head from 'next/head';
-import Image from 'next/image';
-import { Grid, Container, Divider, Typography } from '@mui/material';
+import { useEffect } from "react";
+import { useTheme } from "@mui/material";
+import { useWeb3 } from "@3rdweb/hooks";
+import Header from "../components/Header";
+import Head from "next/head";
+import { Grid, Container, Divider, Typography } from "@mui/material";
 
 const style = {
   wrapper: ``,
@@ -14,10 +12,6 @@ const style = {
   details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
 };
 
-
-
-
-
 export default function Home() {
   const { address, connectWallet } = useWeb3();
   const theme = useTheme();
@@ -25,9 +19,9 @@ export default function Home() {
     if (!address) return;
     (async () => {
       const userDoc = {
-        _type: 'users',
+        _type: "users",
         _id: address,
-        userName: 'Unnamed',
+        userName: "Unnamed",
         walletAddress: address,
       };
     })();
@@ -39,67 +33,52 @@ export default function Home() {
         <>
           <Header />
           <div>
-      <Head>
-        <title>PBandJ</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Container>
-        
-        <Grid 
-          container 
-          direction="column" 
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item xs={12}>
-            <Typography 
-              variant="h1"
-            >
-              p b and j
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography
-              variant="h2"
-            >
-              .
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography
-              variant="h2"
-            >
-              .
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-          <Typography
-              variant="h2"
-            >
-              we'll bring the bread
-            </Typography>
-          </Grid>
-        </Grid>
-        <Divider variant="middle" />
-        <Grid 
-          container 
-          direction="column" 
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item xs={12}>
-           
-          </Grid>
-        </Grid>
-      </Container>
-    </div> 
+            <Head>
+              <title>PBandJ</title>
+              <meta charSet="utf-8" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+            </Head>
+            <Container>
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item xs={12}>
+                  <Typography variant="h1">p b and j</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h2">.</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h2">.</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h2">we'll bring the bread</Typography>
+                </Grid>
+              </Grid>
+              <Divider variant="middle" />
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item xs={12}></Grid>
+              </Grid>
+            </Container>
+          </div>
         </>
       ) : (
         <div className={style.walletConnectWrapper}>
           <button
             className={style.button}
-            onClick={() => connectWallet('injected')}>
+            onClick={() => connectWallet("injected")}
+          >
             Connect Wallet
           </button>
           <div className={style.details}>
@@ -107,4 +86,6 @@ export default function Home() {
           </div>
         </div>
       )}
-      </div>
+    </div>
+  );
+}
