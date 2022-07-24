20c482f52eee7ca74f12a414748c9c6978fe067f
Blake Wright <btdubbzzz@gmail.com>
2022-07-23 22:30:58 -0500

    restyled auth page

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,79 @@
-import { useEffect } from 'react';
-import { useTheme } from '@mui/material';
-import { useWeb3 } from '@3rdweb/hooks';
 import Head from 'next/head';
 import { Grid, Container, Divider, Typography } from '@mui/material';
-import { client } from '../lib/sanityClient';
+import { useTheme, Button } from '@mui/material';
 
-const style = {
-  wrapper: ``,
-  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
-  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
-  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`
-};
-
-export default function Home({ themeColor, setThemeColor }) {
-  const { address, connectWallet } = useWeb3();
+export default function Home() {
   const theme = useTheme();
-
-  useEffect(() => {
-    if (!address) return;
-    (async () => {
-      const userDoc = {
-        _type: 'users',
-        _id: address,
-        userName: 'Unnamed',
-        walletAddress: address
-      };
-      const result = await client.createIfNotExists(userDoc);
-    })();
-  }, [address]);
-
   return (
-    <div className={style.wrapper}>
-      {address ? (
-        <>
-          <div
-            style={{
-              backgroundColor: theme.palette.background.main,
-              height: '100vh'
-            }}
-          >
-            <Head>
-              <title>PBandJ</title>
-              <meta charSet="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <Container>
-              <Grid container direction="column" justifyContent="center" alignItems="center">
-                <Grid item xs={12}>
-                  <Typography variant="h1" color={theme.palette.text.primary}>
-                    p b and j
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="h2" color={theme.palette.text.primary}>
-                    .
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="h2" color={theme.palette.text.primary}>
-                    .
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="h2" color={theme.palette.text.primary}>
-                    we'll bring the bread
-                  </Typography>
-                </Grid>
+    <div>
+      <>
+        <div>
+          <Head>
+            <title>PBandJ</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <Container>
+            <Grid container direction="column" justifyContent="center" alignItems="center">
+              <Grid item xs={12}>
+                <Typography variant="h1" color={theme.palette.text.primary}>
+                  p b and j
+                </Typography>
               </Grid>
-              <Divider variant="middle" />
-              <Grid container direction="column" justifyContent="center" alignItems="center">
-                <Grid item xs={12}></Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  .
+                </Typography>
               </Grid>
-            </Container>
-          </div>
-        </>
-      ) : (
-        <div className={style.walletConnectWrapper}>
-          <button className={style.button} onClick={() => connectWallet('injected')}>
-            Connect Wallet
-          </button>
-          <div className={style.details}>
-            You need Chrome to be <br /> able to run this app.
-          </div>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  .
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  we'll bring the bread
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  .
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  .
+                </Typography>
+              </Grid>
+            </Grid>
+            <Divider variant="middle" />
+          </Container>
+          <Container>
+            <Grid container direction="column" justifyContent="center" alignItems="center">
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  .
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  .
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  Are you lazy?
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h2" color={theme.palette.text.primary}>
+                  Is the most you can manage to make for lunch a PB and J? Great.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
         </div>
-      )}
+      </>
     </div>
   );
 }
