10de47fc8e86a17b03ea8b67efb46873aa6baa6b
Kelsey Gasser <kjgasser@gmail.com>
2022-07-24 08:52:33 -0500

    dis deletin stuff " git push

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import ImageUploader from '../../components/ImageUploader/ImageUploader';
 import ImageGrid from '../../components/ImageGrid/ImageGrid';
-import { getPinata, getAccessToken } from './../../pinata/pinata.js';
+import { getPinata, deletePinata, unSubmarine } from './../../pinata/pinata.js';
 
 export default function Dashboard() {
   const [images, setImages] = useState([]);
@@ -11,6 +11,7 @@ export default function Dashboard() {
       if (images.length === 0) {
         await getPinata()
           .then((res) => {
+            console.log(res.nfts);
             getImageArray(res.nfts, res.accessToken);
           })
           .catch((err) => console.error(err));
@@ -24,10 +25,18 @@ export default function Dashboard() {
     return url;
   };
 
+  const getCid = (url) => {
+    const ending = url.split('/ipfs/')[1];
+    const cid = ending.split('?')[0];
+    console.log(cid);
+    return cid;
+  };
+
   const getImageArray = async (NFTs, accessToken) => {
     const urls = NFTs.map((NFT) => {
       let url = buildImageUrl(NFT.cid, accessToken);
-      return url;
+      let id = NFT.id;
+      return { url, id };
     });
     setImages(urls);
   };
@@ -41,12 +50,32 @@ export default function Dashboard() {
       .catch((err) => console.error(err));
   };
 
+  const deleteImage = async (id, url) => {
+    console.log('dis deletin');
+    const cid = getCid(url);
+    await deletePinata(id, cid).then((res) => {
+      console.log(res);
+      if (res.status === 200) {
+        window.alert('Image Deleted');
+      } else if (res === 'CURRENT_USER_HAS_NOT_PINNED_CID') {
+        window.alert('you have not pinned this cid');
+      }
+    });
+    await getNewImage();
+  };
+
+  const pinIt = async (id) => {
+    await unSubmarine(id).then((res) => {
+      window.alert(res);
+    });
+  };
+
   return (
     <div>
       <br />
       <ImageUploader getNewImage={getNewImage} />
       {images.length > 0 ? (
-        <ImageGrid images={images} />
+        <ImageGrid images={images} deleteImage={deleteImage} pinIt={pinIt} />
       ) : (
         <p>Upload Some Images Above To See Some NFTs</p>
       )}
